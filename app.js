f9ec685069de78b607c132a717eb8de3d5e1c5ca
admin <Mistermo716@gmail.com>
2018-04-04 12:02:07 -0700

    Filtering Array

--- app.js
+++ app.js
@@ -12,6 +12,22 @@ function urlify(string) {
   newString = newString.join('');
   return newString;
 }
-let string = 'www.thinkful.com /tauh ida parv een';
 
-console.log(urlify(string));
+//uncomment lines below to test urlify function
+// let string = 'www.thinkful.com /tauh ida parv een';
+// console.log(urlify(string));
+
+function filterArray(arr, filter) {
+  let newArray = [];
+  let position = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= filter) {
+      newArray[position] = arr[i];
+      position++;
+    }
+  }
+  return newArray;
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(filterArray(array, 3));
