6238c646c973e3cab4f5050f8b5d9f87654277fb
admin <Mistermo716@gmail.com>
2018-04-04 12:32:09 -0700

    add a different urlify algorithim

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 'use strict';
 
 function urlify(string) {
-  let newString = [];
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === ' ') {
-      newString[i] = '%20';
-    } else {
-      newString[i] = string[i];
+    let newString = [];
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === ' ') {
+        newString[i] = '%20';
+      } else {
+        newString[i] = string[i];
+      }
     }
+    newString = newString.join('');
+    return newString;
   }
-  newString = newString.join('');
-  return newString;
-}
+  
+function urlify2(string) {
+    let newString = '';
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === ' ') {
+        newString = newString + '%20';
+      } else {
+        newString = newString + string[i];
+      }
+    }
+    return newString;
+  }
+
 
 //uncomment lines below to test urlify function
 // let string = 'www.thinkful.com /tauh ida parv een';
@@ -29,5 +42,8 @@ function filterArray(arr, filter) {
   return newArray;
 }
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(filterArray(array, 3));
+//Uncomment lines below to test filterArray
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(filterArray(array, 5));
+
+function max
