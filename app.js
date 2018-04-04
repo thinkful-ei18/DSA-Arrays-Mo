94e89de96b379b3ff91ddec05ed7b5babaaf87b4
admin <Mistermo716@gmail.com>
2018-04-04 13:18:44 -0700

    Max Sequental Sum

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 'use strict';
 
 function urlify(string) {
-    let newString = [];
-    for (let i = 0; i < string.length; i++) {
-      if (string[i] === ' ') {
-        newString[i] = '%20';
-      } else {
-        newString[i] = string[i];
-      }
+  let newString = [];
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      newString[i] = '%20';
+    } else {
+      newString[i] = string[i];
     }
-    newString = newString.join('');
-    return newString;
   }
-  
-function urlify2(string) {
-    let newString = '';
-    for (let i = 0; i < string.length; i++) {
-      if (string[i] === ' ') {
-        newString = newString + '%20';
-      } else {
-        newString = newString + string[i];
-      }
-    }
-    return newString;
-  }
+  newString = newString.join('');
+  return newString;
+}
 
+function urlify2(string) {
+  let newString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      newString = newString + '%20';
+    } else {
+      newString = newString + string[i];
+    }
+  }
+  return newString;
+}
 
 //uncomment lines below to test urlify function
 // let string = 'www.thinkful.com /tauh ida parv een';
@@ -46,4 +45,26 @@ function filterArray(arr, filter) {
 // let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // console.log(filterArray(array, 5));
 
-function max
+function maxSeqSum(arr) {
+  let sum = 0;
+  let max = 0;
+  let count = 0;
+  let seqSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (max < sum) {
+      max = sum;
+    }
+    count++;
+    for (let j = count; j < arr.length; j++) {
+      seqSum += arr[j];
+      if (max < seqSum) {
+        max = seqSum;
+      }
+    }
+    seqSum = 0;
+  }
+  return max;
+}
+
+console.log(maxSeqSum([4, -3, -5, -2, 1, 6]));
